244b22b2c8a82e7c412ed404d1574486fc49646c
sunny <sunny.zeelab@gmail.com>
2025-08-22 18:11:12 +0530

    linking pptx invitation

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,23 @@ import WordPuzzle from './component/WordPuzzle';
 import SaveDate from './component/SaveDate';
 
 
-
 function App() {
   const [gameComplete, setGameComplete] = useState(false);
 
+  if (!gameComplete) {
+    return <WordPuzzle onComplete={() => setGameComplete(true)} />;
+  }
+
+  // When game is complete → open ppt
   return (
-    <div>
-      {!gameComplete ? (
-        <WordPuzzle onComplete={() => setGameComplete(true)} />
-      ) : (
-        <SaveDate />
-        
-        
-      )}
+    <div style={{ width: "100%", height: "100vh" }}>
+      <iframe
+        src="https://docs.google.com/viewer?url=https://abbotinvitation.vercel.app/SaveTheDate-AVEIRDR.pptx&embedded=true"
+        style={{ width: "100%", height: "100%" }}
+        frameBorder="0"
+        title="PPT Viewer"
+      ></iframe>
     </div>
   );
 }
-
 export default App;
